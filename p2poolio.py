e7dc8bb0047c75d2f4a3efb3a3bae8975c89c968
Ole <64090338+OlMi1@users.noreply.github.com>
2023-02-15 20:06:39 +0100

    Create p2poolio.py

---
+++ p2poolio.py
@@ -0,0 +1,78 @@
+# the payout variable always holds the most recent one
+# i am a cluttered mess written in like 10 minutes, so don't be too harsh :)
+# data is a json mess (lines only if read is called with returnLines=True). View readme.md for more info
+
+# Options for init: path (default: p2pool.log - must point to p2pool.log)
+# Options for read: path (default: p2pool.log - must point to p2pool.log), returnLines (default: False), linelimit (default: 100)
+
+import time
+import datetime
+import os
+
+class P2PoolIO:
+    def read(path: str = "p2pool.log", returnLines: bool = False, linelimit: int = 100):
+        starttime = time.time()
+
+        if (not os.path.exists(path)):
+            raise Exception('The given path does not exist! Check for {path}')
+        
+        lines = []
+        payout = {}
+
+        possibleTags = ["INFO", "WARN", "ERROR", "FATAL", "DEBUG", "TRACE", "NOTICE"] # thx copilot
+
+        with open(path, "r") as f:
+            # Read the file in reverse and add each line to the list
+            for line in reversed(f.readlines()):
+                if (len(lines) < linelimit):
+                    line = line.replace("\n", "").replace("  ", " ")
+
+                    # READ AND CATEGORIZE (and declutter)
+                    splits = line.split(" ")
+
+                    type = splits[0]
+                    if (type not in possibleTags):
+                        continue
+                    timestamp = int(time.mktime(datetime.datetime.strptime(
+                        f'{splits[1]} {splits[2].split(".")[0]}', "%Y-%m-%d %H:%M:%S").timetuple()))
+                    module = splits[3]
+                    line = " ".join(splits[4:])
+
+                    # Your wallet ... didn't get a payout in block 2814592 because you had no shares in PPLNS window
+                    # Your wallet ... got a payout of 0.001077410546 XMR in block 2814766
+                    hasPayout: bool = (module == "P2Pool" and line.startswith("Your wallet") and "got a payout" in line and not "no shares" in line and payout == {}) 
+                    payoutdata = {
+                        "payout": hasPayout
+                    }
+
+                    if (hasPayout):
+                        payoutdata["amount"] = line.split("got a payout of ")[1].split(" ")[0]
+
+                    added = {
+                        "type": type,
+                        "timestamp": timestamp,
+                        "module": module,
+                        "payout": payoutdata,
+                        "content": line
+                    }
+                    lines.append(added)
+
+                    if (hasPayout):
+                        payoutdata["timestamp"] = timestamp
+                        del(payoutdata["payout"])
+                        payout = payoutdata
+                else:
+                    break
+
+        returndata = {
+            "payout": payout,
+            "time": time.time() - starttime,
+        }
+
+        if (returnLines):
+            returndata["lines"] = lines
+
+        return returndata
+
+
+print(P2PoolIO.read())
